494d070ce0b0509905b1dac38cecc54540809a10
nothing628 <yogyphang@gmail.com>
2023-06-02 22:20:46 +0700

    finish base32 encoding

--- src/base_encoding/base32.v
+++ src/base_encoding/base32.v
@@ -4,14 +4,15 @@ import math
 import strings
 
 const base32_character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+
 const base32_padding = '='
 
 fn convert_str_to_bytes(inp string) []u8 {
 	inp_runes := inp.runes()
-	mut inp_bytes := []u8 {}
+	mut inp_bytes := []u8{}
 
 	for inp_run in inp_runes {
-		inp_bytes<<inp_run.bytes()
+		inp_bytes << inp_run.bytes()
 	}
 
 	return inp_bytes
@@ -23,6 +24,56 @@ fn calculate_bit_count(inp []u8) u64 {
 	return u64(arr_count) * 8
 }
 
+fn floor_div(a u64, b u64) u64 {
+	div_result := f64(a) / f64(b)
+
+	return u64(math.floor(div_result))
+}
+
+fn index_start(a u64) u64 {
+	return floor_div(a, 8)
+}
+
+fn get_masking(c u8) u8 {
+	mut result := 0
+
+	for i in 0 .. c {
+		result |= 1 << i
+	}
+
+	return u8(result)
+}
+
+fn safe_get_array_element(inp []u8, idx u64) u8 {
+	if idx < inp.len {
+		return inp[idx]
+	}
+	return 0
+}
+
+fn get_bit(from u64, inp []u8) u8 {
+	to := from + 5	//5
+	from_index := index_start(from) //0
+	is_need_next := (from_index + 1) * 8 < to // 8 < 5
+
+	elem0 := safe_get_array_element(inp, from_index)
+
+	if !is_need_next {
+		shift_count := (from_index + 1)*8 - to	//
+		out := elem0 >> shift_count
+
+		return out & 0b00011111
+	}
+
+	shift_count0 := to - (from_index + 1) * 8
+	out0 := elem0 << shift_count0
+	elem1 := safe_get_array_element(inp, from_index + 1)
+	shift_count1 := (from_index + 2) * 8 - to
+	out1 := elem1 >> shift_count1
+
+	return (out0 | out1) & 0b00011111
+}
+
 fn calculate_padding_count(bit_count u64) u64 {
 	bit_remain := bit_count % 40
 	last_char_count := math.ceil(f64(bit_remain) / 5)
@@ -31,15 +82,31 @@ fn calculate_padding_count(bit_count u64) u64 {
 }
 
 fn produce_padding(count int) string {
-	return strings.repeat(base32_padding[0], count)
+	return strings.repeat(base_encoding.base32_padding[0], count)
 }
 
-pub fn encode_base32(inp string) {
+fn lookup_char(idx u8) string {
+	if idx >= base_encoding.base32_character.len {
+		return ''
+	}
+
+	return base_encoding.base32_character[idx].ascii_str()
+}
+
+pub fn encode_base32(inp string) string {
 	inp_bytes := convert_str_to_bytes(inp)
 	inp_bit_count := calculate_bit_count(inp_bytes)
 	padding_count := calculate_padding_count(inp_bit_count)
 	padding := produce_padding(int(padding_count))
+	mut str_builder := strings.new_builder(0)
+	mut current_bit := u64(0)
 
-	println(padding)
+	for current_bit < inp_bit_count {
+		out := get_bit(current_bit, inp_bytes)
+		str_builder.write_string(lookup_char(out))
+		current_bit += 5
+	}
+
+	str_builder.write_string(padding)
+	return str_builder.str()
 }
-
--- src/base_encoding/base32_test.v
+++ src/base_encoding/base32_test.v
@@ -9,19 +9,19 @@ fn test_base32_character_count() {
 fn test_convert_str_to_bytes() {
 	mut result := convert_str_to_bytes('hihi')
 
-	assert result == [u8(104),105,104,105]
+	assert result == [u8(104), 105, 104, 105]
 	result = convert_str_to_bytes('axax')
 
-	assert result == [u8(97),120,97,120]
+	assert result == [u8(97), 120, 97, 120]
 }
 
 fn test_calculate_bit_count() {
-	mut data := [] u8  {}
+	mut data := []u8{}
 
-	data<<0x24
+	data << 0x24
 	assert calculate_bit_count(data) == 8
 
-	data<<0xFF
+	data << 0xFF
 	assert calculate_bit_count(data) == 16
 }
 
@@ -33,13 +33,35 @@ fn test_calculate_padding_count() {
 }
 
 fn test_produce_padding() {
-	assert  produce_padding(1) == '='
-	assert  produce_padding(4) == '===='
-	assert  produce_padding(6) == '======'
-	assert  produce_padding(8) == '========'
+	assert produce_padding(1) == '='
+	assert produce_padding(4) == '===='
+	assert produce_padding(6) == '======'
+	assert produce_padding(8) == '========'
+}
+
+fn test_lookup_char() {
+	assert lookup_char(0) == 'A'
+	assert lookup_char(9) == 'J'
+	assert lookup_char(16) == 'Q'
+	assert lookup_char(24) == 'Y'
+	assert lookup_char(28) == '4'
+	assert lookup_char(31) == '7'
+}
+
+fn test_get_bit() {
+	inp := [u8(0b01001010), 0b11001010, 0b10111101, 0b11100100, 0b00001010]
+
+	assert get_bit(0 * 5, inp) == 0b00001001
+	assert get_bit(1 * 5, inp) == 0b00001011
+	assert get_bit(2 * 5, inp) == 0b00000101
+	assert get_bit(3 * 5, inp) == 0b00001011
+	assert get_bit(4 * 5, inp) == 0b00011011
+	assert get_bit(5 * 5, inp) == 0b00011001
+	assert get_bit(6 * 5, inp) == 0b00000000
+	assert get_bit(7 * 5, inp) == 0b00001010
 }
 
 fn test_base32_encode_function() {
-	encode_base32('hello world')
-	assert true
+	result := encode_base32('hello world')
+	assert result == 'NBSWY3DPEB3W64TMMQ======'
 }
